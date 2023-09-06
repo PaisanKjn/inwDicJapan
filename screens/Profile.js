dcdda43f526e0b39cd5de3e59db29f57e8184a97
JvdeHub <pt.jvde@gmail.com>
2023-09-06 21:01:20 +0700

    JvdeHub

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,5 +1,5 @@
 import React from 'react' 
-import { View, Text, TouchableOpacity, TextInput , StyleSheet} from 'react-native'
+import { View, Text, TouchableOpacity, TextInput , StyleSheet, Image} from 'react-native'
 import {PropsWithChildren} from 'react'
 import { useState, useEffect } from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -15,17 +15,55 @@ const Profile = ({navigation, route}) => {
   
 
   return (
-    <View>
-      <Text>Hello {user}</Text>
-      <TouchableOpacity
-      onPress={handleOnLogOut}
-      >
-        <Text>Log Out</Text>
+    <View style={[{ backgroundColor: "#0e0e0e", flex: 1 }]}>
+      <View style={[{ flexDirection: 'row'}]}>
+      <TouchableOpacity style={[styles.circle,{ marginTop: 50, marginLeft: 40}]} className="text-xl">
+      <Image
+                source={{
+                  uri: "https://reactnative.dev/img/tiny_logo.png",
+                }}
+                style={{width: 80, height: 80, margin: 10, resizeMode: 'center'}}
+              />
       </TouchableOpacity>
+      <Text style={styles.head}>Username</Text>
+      </View>
+      <View>
+      <TouchableOpacity
+          onPress={() => {
+            handleSubmit();
+          }}
+        >
+          <Text style={[styles.button,{marginLeft: 240, marginTop: 100}]} className="text-xl">
+            Logout
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
 
 export default Profile
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  head: {
+    textAlign: "center",
+    color: "white",
+    fontSize: 24,
+    marginTop: 75,
+    marginLeft: 50,
+    fontWeight: '500'
+  },
+  button: {
+    textAlign: "center",
+    fontSize: 24,
+    fontWeight: "500",
+    backgroundColor: "white",
+    width: 100,
+    height: 35,
+    borderRadius: 100
+  },
+  circle: {
+    textAlign: "center",
+    backgroundColor: "white"
+  }
+})
