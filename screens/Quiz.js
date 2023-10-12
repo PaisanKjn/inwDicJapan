b3029a1ad147d8c86b4d01dc03539967a17924b7
JvdeHub <pt.jvde@gmail.com>
2023-10-12 23:15:00 +0700

    Jade

--- screens/Quiz.js
+++ screens/Quiz.js
@@ -70,8 +70,9 @@ const Quiz = ({ navigation, route }) => {
 
   const fetchWord = async () => {
     try {
-      const response = await fetch('http://localhost:8080/wordlist?jlpt=N' + route.params?.difficulty + '&row=10')
+     const response = await fetch('http://192.168.1.100:8080/wordlist?jlpt=N' + route.params.difficulty + '&row=10')
      const data = await response.json();
+     console.log(data)
      setListItems(data)
      setWord(listItems[0].vocab)
      setLoading(false)
